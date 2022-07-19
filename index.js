517c22da5fcb9a110465d40541d8b91f31a66f38
Roger Earl <rogerearl@me.com>
2022-07-18 19:46:39 -0700

    added js code

--- index.js
+++ index.js
@@ -2,4 +2,28 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+// Included Arrays
+// weeklyExpenseQuestions - array
+// monthlyExpenseQuestions
+// annualExpenseQuestions
+
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let firstAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    console.log(firstAnswer);
+    weeklyExpenses = weeklyExpenses + firstAnswer;
+    console.log(weeklyExpenses)
+}
+
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let firstAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+    console.log(firstAnswer);
+    monthlyExpenses = monthlyExpenses + firstAnswer;
+    console.log(monthlyExpenses)
+}
+
+for (let i = 0; i < annualExpenseQuestions.length; i++) {
+    let firstAnswer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+    console.log(firstAnswer);
+    annualExpenses = annualExpenses + firstAnswer;
+    console.log(annualExpenses)
+}
